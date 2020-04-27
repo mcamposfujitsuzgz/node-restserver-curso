20e8f2e69b5455d8f31835ee232ad37f160cd517
Manuel Campos <mcampos.fujitsu.zgz@gmail.com>
2020-04-27 09:58:29 +0200

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,8 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mcampos:lbTdWarFRyT4bn7G@cluster0-yqz1n.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
-//urlDB = 'mongodb+srv://mcampos:lbTdWarFRyT4bn7G@cluster0-yqz1n.mongodb.net/cafe';
 process.env.URL_DB = urlDB;
